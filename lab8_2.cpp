553b3c768075504aeaedf04137d5c1f1e3b516a8
Jarkrasri Thonglueng <72305051+klyemeo@users.noreply.github.com>
2025-12-16 15:42:11 +0700

    kod kai
    Implemented the findGrade function to determine letter grades based on numeric scores.

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,7 +4,19 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
-
+char findGrade(double grade){
+	if (grade > 90.0){
+		 return  'A' ; 
+	}else if (grade > 75.0 && grade <= 90.0){
+		 return  'B' ; 
+	}else if (grade > 60 && grade <= 75){
+		 return  'C' ; 
+	}else if (grade > 45 && grade <= 60){
+		 return  'D' ;
+	}else{
+		 return  'F' ; 
+	}
+}
 int main(){
 	//Input the number of students
 	int N,i = 0;
@@ -14,11 +26,18 @@ int main(){
 	float score[N];	
 	
 	//Store names and scores of students into an array 
-	while(i < N){
-		cout << "Name of student " << i+1 << ": ";
+	while(i < N){ 
+		cout << "Name of student " << i+1 << ": " ;
 		cin.ignore();
+		getline(cin, name[i]);
+		//cin >> name[i];
+		//cin.ignore();
+
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
 		cout << "Score of student " << i+1 << ": ";
+		cin >> score[i];
+		
+
 		//[Missing Code 3] Get score of the i-th students.
 		i++;
 	}
