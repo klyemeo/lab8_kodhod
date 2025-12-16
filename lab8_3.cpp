ef45c1fe0d4244f530b2ee7da4272f9f9d66f88a
Jarkrasri Thonglueng <72305051+klyemeo@users.noreply.github.com>
2025-12-16 15:42:35 +0700

    freaky ahhh

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,43 @@
 "Brook"
 "Jinbe"
 */
+
+
+
+#include <iostream>
+
+using namespace std; 
+
+int main() {
+    double age ; double height ;  double rate ; 
+    cout << "Enter your age: " ; 
+    cin >> age ;
+    if (age <= 25){
+        cout << "Enter your height: " ; 
+        cin >> height ; 
+        if (height < 100){
+            cout << "Your character = Chopper" ; 
+        }else if (height < 180) {
+            cout << "Your character = Usopp" ; 
+        }else {
+            cout << "Enter your bounty: ";
+            cin >> rate ; 
+            if (rate > 1100000000){
+                cout << "Your character = Zoro" ;
+            }else {
+                cout << "Your character = Sanji";
+            }
+        }
+    }else if (age <= 60 ) {
+        cout << "Enter your bounty: ";
+        cin >> rate ;  
+        if (rate > 500000000){
+            cout << "Your character = Jinbe" ; 
+        }else {
+            cout << "Your character = Franky" ;
+        }
+
+    }else {
+        cout << "Your character = Brook" ; 
+    }
+}
